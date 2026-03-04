98bdcf49e3a750fff922c39c11c331800d9fddbe
yangweiguang <yangwg0809@163.com>
2026-03-04 10:15:22 +0800

    change db_version to 2

--- rust/attest/src/constants.rs
+++ rust/attest/src/constants.rs
@@ -124,7 +124,7 @@ pub const RAFT_CONFIG_SVR2_STAGING: &RaftConfig = &RaftConfig {
     max_voting_replicas: 9,
     super_majority: 2,
     group_id: 9161153614836317716,
-    db_version: 4,
+    db_version: 2,
     attestation_timeout: 604800,
     simulated: false,
 };
@@ -144,7 +144,7 @@ pub const RAFT_CONFIG_SVRB_STAGING: &RaftConfig = &RaftConfig {
     max_voting_replicas: 9,
     super_majority: 0,
     group_id: 16478507710842799467,
-    db_version: 2,
+    db_version: 4,
     attestation_timeout: 604800,
     simulated: false,
 };
